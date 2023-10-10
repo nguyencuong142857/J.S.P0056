f2a75db7e6e69a86cd8fc8dd4d62f493c50f5c09
NguyenCuong <111842969+nguyencuong142857@users.noreply.github.com>
2023-10-10 21:38:36 +0700

    Create Constant.java

---
+++ J.S.P0056/src/constans/Constant.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package constans;
+
+/**
+ *
+ * @author ngnqu
+ */
+public class Constant {
+    public static final String REGEX_CODE = "^[A-Za-z0-9 ]*$";
+    public static final String REGEX_NAME = "^[A-Za-z\\s]*$";
+    public static final String REGEX_SPECIALIZATION = "^[A-Za-z\\s ]*$";
+}
